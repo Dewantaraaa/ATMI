0d827fabfe28744b39c9557401cb23de34936473
Dewantaraaa <102471798+Dewantaraaa@users.noreply.github.com>
2025-08-14 13:49:28 +0700

    Update project.h

--- PROGRAMING/PROJECT BIOSKOP/project.h
+++ PROGRAMING/PROJECT BIOSKOP/project.h
@@ -37,24 +37,24 @@ public:
 	vector<vector<vector<bool>>> kursiStatus;
 
 	// Fungsi untuk registrasi user baru
-	void registerUser();
+	void registerUser(); //denyar
 	// Fungsi untuk login user/admin
-	bool login(string& loggedInUser, bool& isAdmin);
+	bool login(string& loggedInUser, bool& isAdmin); //denyar
 	// Fungsi untuk menambah film baru
-	void tambahFilm();
+	void tambahFilm(); //dewa
 	// Fungsi untuk menampilkan daftar film
-	void tampilkanFilm();
+	void tampilkanFilm(); //denyar
 	// Fungsi untuk menampilkan status kursi pada studio
-	void tampilkanKursi(const vector<vector<bool>>& kursi);
+	void tampilkanKursi(const vector<vector<bool>>& kursi); //dewa
 	// Fungsi untuk mengedit data film
-	void editFilm();
+	void editFilm(); //santi
 	// Fungsi untuk menghapus film dari daftar
-	void hapusFilm();
+	void hapusFilm(); //santi
 	// Fungsi untuk menampilkan nota pembelian tiket
-	void tampilkanNota(const Film& film, const vector<string>& kursiDipilih, int jumlahTiket);
+	void tampilkanNota(const Film& film, const vector<string>& kursiDipilih, int jumlahTiket); //gawok
 	// Fungsi untuk mencetak nota pembelian ke file txt
-	void printNotaTxt(const Film& film, const vector<string>& kursiDipilih, int jumlahTiket);
+	void printNotaTxt(const Film& film, const vector<string>& kursiDipilih, int jumlahTiket); //dewa
 	// Fungsi untuk proses pemilihan film dan pembelian tiket
-	void pilihFilm();
+	void pilihFilm(); //gawok
 };
 
