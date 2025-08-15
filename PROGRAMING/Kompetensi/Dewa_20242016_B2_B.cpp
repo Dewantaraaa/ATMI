a0f77c3461220fb935f6a54d869f771231d64510
Dewantaraaa <102471798+Dewantaraaa@users.noreply.github.com>
2025-08-15 13:55:06 +0700

    Create Dewa_20242016_B2_B.cpp

---
+++ PROGRAMING/Kompetensi/Dewa_20242016_B2_B.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
+#include <map>
+#include <conio.h> 
+#include <fstream>
+#include <ctime>
+#include "kompetensi.h"
+
+using namespace std;
+
+int main()
+{
+    kompetensi ob;
+    ob.landSpace();
+}
